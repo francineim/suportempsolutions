3facc454692ff2d8fabcd773e0f831d2e2143b55
Francinei Marques <francinei@mpsolutions.com.br>
2025-12-20 09:47:58 -0300

    Implement user authentication and creation functions

--- app/auth.py
+++ app/auth.py
@@ -1,1 +1,28 @@
+from app.database import conectar
 
+
+def autenticar(usuario, senha):
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT usuario, perfil FROM usuarios WHERE usuario=? AND senha=?",
+        (usuario, senha)
+    )
+
+    user = cur.fetchone()
+    conn.close()
+    return user
+
+
+def criar_usuario(usuario, senha, perfil="cliente"):
+    conn = conectar()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO usuarios (usuario, senha, perfil) VALUES (?, ?, ?)",
+        (usuario, senha, perfil)
+    )
+
+    conn.commit()
+    conn.close()
