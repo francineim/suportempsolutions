62440e17f19f75a442326c71547f01fc8b480528
Francinei Marques <francinei@mpsolutions.com.br>
2025-12-20 10:38:28 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+from database import criar_tabelas
+from auth import login
+from chamados import tela_chamados
+from dashboard import tela_dashboard
+
+st.set_page_config(
+    page_title="Helpdesk MP Solutions",
+    layout="wide"
+)
+
+def main():
+    criar_tabelas()
+
+    usuario_logado = login()
+
+    if usuario_logado:
+        menu = st.sidebar.selectbox(
+            "Menu",
+            ["Chamados", "Dashboard"]
+        )
+
+        if menu == "Chamados":
+            tela_chamados(usuario_logado)
+
+        elif menu == "Dashboard":
+            tela_dashboard()
+
+if __name__ == "__main__":
+    main()
